b8f1391a022cd835d7701dbadbbf6d2978080aee
Andrew Newton <andy@hxr.us>
2019-10-17 10:44:54 -0400

    more fleshed out

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,8 @@ import {
 } from 'react-router-dom';
 import Todo from './pages/Todo';
 
-interface PrivateRouteParams {
-  children: ReactNode,
-  rest: { [x: string]: any }
+interface PrivateRouteProps {
+  path: string
 }
 
 class App extends Component<{}> {
@@ -49,13 +48,66 @@ class App extends Component<{}> {
     );
   }
 
-
 }
 
-export const PrivateRoute: FunctionComponent<PrivateRouteParams> = (params) =>
-      <Route {...params.rest} render={({ location }) => 
-        fakeAuth.isAuthenticated ? ( params.children ) : ( <Redirect to={{ pathname: "/login", state: { from: location } }}/>)
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb : () => void ) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb,100);
+  },
+  signout(cb : () => void ) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb,100);
+  }
+}
+
+export const PrivateRoute: FunctionComponent<PrivateRouteProps> = (props) =>
+      <Route render={({ location }) => 
+        fakeAuth.isAuthenticated ? ( props.children ) : ( <Redirect to={{ pathname: "/login", state: { from: location } }}/>)
         } />
 
+export const AuthButton = ({}) => {
+  let history = useHistory();
+
+  return fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome!{" "}
+      <button
+        onClick={()=> {
+          fakeAuth.signout(() => history.push("/"));
+        }}
+        >
+          Sign out
+        </button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  )
+}
+
+
+export const PublicPage = ({}) => {
+  return <h3>Public</h3>;
+}
+
+export const LoginPage = ({}) => {
+  let history = useHistory();
+  let location = useLocation();
+
+  let {from} = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    fakeAuth.authenticate( () => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>you must be logged in to view the page {from.pathname}</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  );
+}
 
 export default App;
